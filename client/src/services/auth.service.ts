f68c230f0ed8735843fda7a49b2f62efd26ededc
username <wecp-dummy@gmail.com>
2024-02-24 09:44:52 +0000

    wecp initial commit

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -14,25 +14,35 @@ export class AuthService {
   saveToken(token: string) {
     //complete this function
    //complete this function
+   localStorage.setItem('token', token);
   }
    SetRole(role:any)
   {
    //complete this function
+   localStorage.setItem('role', role);
   }
   get getRole ():string|null
   {
    //complete this function
+   return localStorage.getItem('role');
   }
   // Method to retrieve login status
   get getLoginStatus(): boolean {
   
      //complete this function
+     return !!localStorage.getItem('token');
    
   }
   getToken(): string | null {
  //complete this function
+ this.token = localStorage.getItem('token');
+ return this.token;
   }
   logout(){
   //complete this function
+  localStorage.removeItem('token');
+  localStorage.removeItem('role');
+  this.token=null;
+  this.isLoggedIn=false
    }
 }
